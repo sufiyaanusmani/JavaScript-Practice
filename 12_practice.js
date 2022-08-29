ee019bcc33e4cc955cffb5a616035b5bcff3f6b5
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-08-29 23:06:59 +0500

    day 3

--- 12_practice.js
+++ 12_practice.js
@@ -12,3 +12,23 @@ for (i in marks) {
     sum += marks[i];
 }
 console.log("Sum: " + sum);
+
+// Question 3
+const num = 7;
+let userInput;
+while (true) {
+    console.log("Enter a number: ");
+    userInput = Number.parseInt(prompt());
+    if (num == userInput) {
+        break;
+    } else {
+        console.log("Invalid Number");
+    }
+}
+
+// Question 4
+const mean = (a, b, c) => {
+    return ((a + b + c) / 3);
+}
+
+console.log(mean(5, 4, 8));
